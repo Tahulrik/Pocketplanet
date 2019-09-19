449f176d779a12ca4123c53d6cff7ebb0b209536
Ulrik Kroge Sloth <UlrikKSloth@outlook.com>
2019-09-19 23:29:33 +0200

    finally got the setup to work

--- Assets/Scripts/WorldGeneration/MeshGenerator.cs
+++ Assets/Scripts/WorldGeneration/MeshGenerator.cs
@@ -4,11 +4,52 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-    private static Mesh GenerateCircleMesh()
-    {
-        
+    public int CircleSegmentCount = 64;
 
+    MeshFilter mesh;
+
+    public void Start()
+    {
+        mesh = GetComponent<MeshFilter>();
+    }
+    public void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mesh.mesh = GenerateCircleMesh(CircleSegmentCount);
+        }
     }
 
+    private static Mesh GenerateCircleMesh(int segments)
+    {
+        int CircleVertexCount = segments + 2;
+        int CircleIndexCount = segments * 3;
 
+        var circle = new Mesh();
+        var vertices = new List<Vector3>(CircleVertexCount);
+        var indices = new int[CircleIndexCount];
+        var segmentWidth = Mathf.PI * 2f / segments;
+        var angle = 0f;
+        vertices.Add(Vector3.zero);
+        for (int i = 1; i < CircleVertexCount; ++i)
+        {
+            vertices.Add(new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)));
+            angle -= segmentWidth;
+            if (i > 1)
+            {
+                var j = (i - 2) * 3;
+                indices[j + 0] = 0;
+                indices[j + 1] = i - 1;
+                indices[j + 2] = i;
+            }
+        }
+        circle.SetVertices(vertices);
+        circle.SetIndices(indices, MeshTopology.Triangles, 0);
+        circle.RecalculateBounds();
+
+        circle.name = "CircleMesh";
+        return circle;
+    }
 }
