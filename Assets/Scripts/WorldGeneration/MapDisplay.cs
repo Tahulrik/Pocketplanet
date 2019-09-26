ae8c2cead33bfecc8fea7624206ff4535255b447
Ulrik Kroge Sloth <UlrikKSloth@outlook.com>
2019-09-26 17:19:07 +0200

    Added ability to create a "block world" So far all "continents" will be consisting of equally sized blocks - perhaps they should vary in size

---
+++ Assets/Scripts/WorldGeneration/MapDisplay.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+
+    public Renderer heightMapRend;
+
+    public void DrawNoiseMap(float[,] dataArray, bool thresholdValues)
+    {
+        int width = dataArray.GetLength(0);
+        int height = dataArray.GetLength(1);
+
+        Texture2D mainTexture = new Texture2D(width, height);
+        Texture2D specularMap = new Texture2D(width, height);
+
+        Color[] colourMap = new Color[width * height];
+        Color[] specColourMap = new Color[width * height];
+
+        Color water = Color.blue;
+        Color land = Color.green;
+
+        Color reflective = new Color(0, 0, 1, 1);
+        Color nonReflective = new Color(0, 1, 0, 0);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (thresholdValues)
+                {
+                    colourMap[y * width + x] = (dataArray[x, y] < 0.5f) ? land : water;
+                    specColourMap[y * width + x] = (dataArray[x, y] < 0.5f) ? nonReflective : reflective;
+                }
+                else
+                {
+                    colourMap[y * width + x] = Color.Lerp(land, water, dataArray[x, y]);
+                    specColourMap[y * width + x] = Color.Lerp(nonReflective, reflective, dataArray[x, y]);
+                }
+
+            }
+        }
+
+        mainTexture.SetPixels(colourMap);
+        mainTexture.Apply();
+        mainTexture.filterMode = FilterMode.Point;
+        mainTexture.wrapMode = TextureWrapMode.Clamp;
+
+        specularMap.SetPixels(specColourMap);
+        specularMap.Apply();
+        specularMap.filterMode = FilterMode.Point;
+        specularMap.wrapMode = TextureWrapMode.Clamp;
+
+        heightMapRend.sharedMaterial.mainTexture = mainTexture;
+        heightMapRend.sharedMaterial.SetTexture("_SpecGlossMap",specularMap);
+    }
+}
--- Assets/Scripts/WorldGeneration/MapGenerator.cs
+++ Assets/Scripts/WorldGeneration/MapGenerator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class MapGenerator : MonoBehaviour
 {
     public int CircleSegmentCount = 64;
@@ -9,16 +11,29 @@ public class MapGenerator : MonoBehaviour
     MeshFilter filter;
     Renderer rend;
 
-    [Range(0,1000)]
-    public int MapWidth = 100;
-    [Range(0,1000)]
-    public int MapHeight = 100;
-    [Range(0f, 1f)]
-    public float MapResolution = 0.6f;
+    [Range(1f, 100f)]
+    public int LandmassBlockSize = 1;
 
+    [Range(5f, 50f)]
+    public float NoiseScale = 25f;
+
+    [Range(1, 12)]
+    public int Octaves = 4;
+    [Range(0.1f, 10f)]
+    public float Persistance = 0.5f;
+    [Range(0.1f, 10f)]
+    public float Lacunarity = 2.0f;
 
     [Range(1f, 100f)]
-    public float radius;
+    public float Radius = 3;
+    [Range(0, 1000)]
+    public int PixelDensity = 100;
+
+    public int Seed;
+    public Vector2 Offset;
+    public bool ThresholdMap = false;
+    public bool AutoUpdate = false;
+    public bool SquarizeWorld = false;
 
     public void Start()
     {
@@ -33,43 +48,82 @@ public class MapGenerator : MonoBehaviour
         {
             filter = gameObject.AddComponent<MeshFilter>();
         }
+
+
+
+        //filter.mesh = ;
+
     }
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        // if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            var mapData = NoiseGenerator.GenerateNoiseMap(MapWidth, MapHeight, MapResolution);
 
-            var tex = GenerateMapTexture(mapData);
-            filter.mesh = GenerateCircleMesh(CircleSegmentCount, radius);
-            rend.sharedMaterial.mainTexture = tex;
 
         }
     }
 
-
-
-
-    private Texture2D GenerateMapTexture(float[,] dataArray)
+    public void Generate()
     {
-        int width = dataArray.GetLength(0);
-        int height = dataArray.GetLength(1);
+        var mapData = NoiseGenerator.GenerateNoiseMap((int)Radius * PixelDensity, (int)Radius * PixelDensity, Seed, NoiseScale, Octaves, Persistance, Lacunarity, Offset);
+        if(SquarizeWorld)
+            mapData = SquarizeWorldData(mapData, LandmassBlockSize);
 
-        Texture2D resultTexture = new Texture2D(width, height);
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+
         
-        Color[] colourMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
+        var mesh = display.gameObject.GetComponentInChildren<MeshFilter>();
+        if(mesh.sharedMesh == null)
+            mesh.sharedMesh = GenerateCircleMesh(CircleSegmentCount, Radius);
+
+        display.DrawNoiseMap(mapData, ThresholdMap);
+    }
+
+    private float[,] SquarizeWorldData(float[,] mapData, int kernelSize)
+    {
+        float[,] squarizedMap = (float[,])mapData.Clone();
+        int mapX = mapData.GetLength(0);
+        int mapY = mapData.GetLength(1);
+
+
+        for (int y = 0; y < mapY; y += (kernelSize*2)+1)
         {
-            for(int x = 0; x < width; x++)
+            for (int x = 0; x < mapX; x += (kernelSize*2)+1)
             {
-                colourMap[y * width + x] = dataArray[x,y] < 0.5f ? Color.white : Color.black;
+                squarizedMap = ApplySquareKernel(squarizedMap, x, y, kernelSize);
             }
         }
 
-        resultTexture.SetPixels(colourMap);
-        resultTexture.Apply();
-        
-        return resultTexture;
+        return squarizedMap;
+    }
+
+    private float[,] ApplySquareKernel(float[,] inputMap, int x, int y, int kernelSize)
+    {
+        float[,] outputMap = (float[,])inputMap.Clone();
+        int mapX = outputMap.GetLength(0);
+        int mapY = outputMap.GetLength(1);
+
+        int kernelCenterX = x;
+        int kernelCenterY = y;
+        float resultval = inputMap[kernelCenterX, kernelCenterY] < 0.5f ? 0f : 1f;
+        for (int kernelY = -kernelSize; kernelY <= kernelSize * 2; kernelY++)
+        {
+            int kernelPosY = kernelCenterY + kernelY;
+            if (kernelPosY < 0 || kernelPosY >= mapY)
+                continue;
+
+            for (int kernelX = -kernelSize; kernelX <= kernelSize * 2; kernelX++)
+            {
+                int kernelPosX = kernelCenterX + kernelX;
+
+                if (kernelPosX < 0 || kernelPosX >= mapX)
+                    continue;
+
+                outputMap[kernelPosX, kernelPosY] = resultval;
+            }
+        }
+
+        return outputMap;
     }
 
     private Mesh GenerateCircleMesh(int segments, float radius)
@@ -81,7 +135,6 @@ public class MapGenerator : MonoBehaviour
         var vertices = new List<Vector3>(CircleVertexCount);
         var indices = new int[CircleIndexCount];
         Vector2[] uvs = new Vector2[CircleVertexCount];
-        print(uvs.Length);
         var segmentWidth = Mathf.PI * 2f / segments;
         var angle = 0f;
         vertices.Add(Vector3.zero);
